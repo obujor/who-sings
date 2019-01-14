0928fe1cc12fd588a7649719e0424927c35fee17
Octavian Bujor <octavian.bujor@unibo.it>
2019-01-14 23:13:16 +0100

    Added simple welcomePage

---
+++ src/components/WelcomePage.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+
+const styles = theme => ({
+  root: {
+    padding: theme.spacing.unit * 3,
+    flexGrow: 1,
+    overflow: 'hidden',
+  },
+  startButton: {
+    margin: theme.spacing.unit * 3
+  }
+});
+
+class WelcomePage extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.handleStart = this.handleStart.bind(this);
+
+    this.state = {
+      username: '',
+    };
+  }
+
+  setUsername = event => {
+    this.setState({
+      username: event.target.value,
+    });
+  }
+
+  handleStart() {
+    const { startGame, setUsername } = this.props;
+    if (this.state.username) {
+      setUsername(this.state.username);
+    }
+    startGame();
+  }
+
+  render() {
+    const {
+      classes,
+      username: loggedUsername
+    } = this.props;
+    const { username } = this.state;
+    const canStart = !!(loggedUsername || username);
+    return (
+      <div className={classes.root}>
+        <Typography variant="h4" color="inherit">
+          you have to guess...
+        </Typography>
+        {loggedUsername ? (
+          <div>
+            <Typography variant="h4" color="inherit">
+              Welcome back
+            </Typography>
+            <Typography variant="h4" color="primary">
+              {loggedUsername}
+            </Typography>
+          </div>
+        ) : (
+          <div>
+            <Typography variant="h5" color="inherit">
+              Insert your name
+            </Typography>
+            <TextField
+              id="outlined-name"
+              label="Name"
+              className={classes.textField}
+              value={username}
+              onChange={this.setUsername}
+              margin="normal"
+              variant="outlined"
+            />
+          </div>
+        )}
+        <Button
+          variant="contained"
+          color="primary"
+          className={classes.startButton}
+          onClick={this.handleStart}
+          disabled={!canStart}
+          size="large"
+        >
+          Let's Play
+        </Button>
+      </div>
+    );
+  }
+}
+
+export default withStyles(styles)(WelcomePage);
