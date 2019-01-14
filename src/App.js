243d7e15828a4698e64277ed61f884d4df22321c
Octavian Bujor <octavian.bujor@unibo.it>
2019-01-14 23:15:11 +0100

    Added auth in app and some pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,89 @@
 import React, { Component } from 'react';
 
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, withRouter } from 'react-router-dom';
 
 import './App.css';
 import Header from './components/Header';
+import WelcomePage from './components/WelcomePage';
 import QuizPage from './components/QuizPage';
 import UserPage from './components/UserPage';
+import PrivateRoute from './PrivateRoute';
+
+import { getLoggedUser, login, logout } from './Auth';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: getLoggedUser() || '',
+    };
+  }
+
+  startGame = () => {
+    this.props.history.push('/quiz');
+  }
+
+  setUsername = (username) => {
+    this.setState({username});
+  }
+
+  componentDidUpdate(prevProp, prevState) {
+    if (prevState.username !== this.state.username) {
+      if (this.state.username) {
+        login(this.state.username);
+      } else {
+        logout();
+      }
+    }
+  }
+
+  onLogout = () => {
+    this.setUsername('');
+  }
+
   render() {
+    const username = this.state.username;
+    const isLoggedIn = !!username;
     return (
       <div className="App">
         <Header />
         <Switch>
-          <Route path="/" exact component={QuizPage}/>
-          <Route path="/user" exact component={UserPage}/>
+          <Route path="/" exact render={(props) => (
+            <WelcomePage
+              startGame={this.startGame}
+              setUsername={this.setUsername}
+              username={username}
+            />
+          )}/>
+          <PrivateRoute
+            path="/quiz"
+            exact
+            loginPath="/"
+            isLoggedIn={isLoggedIn}
+            render={() => (
+              <QuizPage
+                nrQuestions={5}
+              />
+            )}
+          />
+          <PrivateRoute
+            path="/user"
+            exact
+            loginPath="/"
+            isLoggedIn={isLoggedIn}
+            render={() => (
+              <UserPage
+                username={username}
+                logout={this.onLogout}
+              />
+            )}
+          />
         </Switch>
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
